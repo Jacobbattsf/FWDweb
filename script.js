2bb18ce9cfbdeabaa2b316f8831072b3566ea1dc
Jacob Battenfield <jacobbattenfieldsf@gmail.com>
2024-03-09 17:08:10 -0600

    Form is working now, style alterations

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var cat = "meow";
-console.log(cat);
+document.getElementById('contactForm').addEventListener('submit',function(event){
+
+var email = document.getElementById('email').value;
+var confirmEmail = document.getElementById('confirmEmail').value;
+var wrongemail = document.getElementById('errormessage');
+
+if(email !== confirmEmail){
+wrongemail.style.display = 'block';
+wrongemail.style.fontSize = "15px"
+event.preventDefault();
+}else{
+    wrongemail.style.display = 'none';
+}
+});
